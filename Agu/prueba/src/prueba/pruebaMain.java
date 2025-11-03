999eac547e8ec56b708b3f35fecf3bd78d63ca18
aguykeny <agualcaldereyes@gmail.com>
2025-11-03 08:51:03 +0100

    a
    a

--- Agu/prueba/src/prueba/pruebaMain.java
+++ Agu/prueba/src/prueba/pruebaMain.java
@@ -4,7 +4,7 @@ public class pruebaMain {
 
 	public static void main(String[] args) {
 		System.out.println("Prueba holaaaa");
-		System.out.println("Pruebaaaaa");
+		System.out.println("probando otra cosa");
 
 	}
 
